8c25f820429fdd0d8fbf3b30e55c94b239022281
Leann Kim <156999284+lhkim6065@users.noreply.github.com>
2024-06-09 17:49:04 -0700

    Update reducer.py

--- reducer.py
+++ reducer.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
 # -*-coding:utf-8 -*
-import sys
-
+  
+from operator import itemgetter 
+import sys 
+  
 current_word = None
 current_count = 0
 word = None
 
-# Read input from standard input
-for line in sys.stdin:
-    # Parse the input we got from mapper.py
-    word, count = line.strip().split('\t', 1)
-    try:
-        count = int(count)
-    except ValueError:
+for line in sys.stdin: 
+    line = line.strip() 
+    word, count = line.split('\t', 1) 
+    try: 
+        count = int(count) 
+    except ValueError: 
         continue
-
-    # If this is the same word as the current word, increment the count
-    if current_word == word:
-        current_count += count
-    else:
-        if current_word:
-            # Write result to standard output
-            print(f"{current_word}\t{current_count}")
-        current_word = word
-        current_count = count
-
-# Output the last word if needed
-if current_word == word:
-    print(f"{current_word}\t{current_count}")
+    if current_word == word: 
+        current_count += count 
+    else: 
+        if current_word: 
+            print('%s\t%s' % (current_word, current_count))
+        current_count = count 
+        current_word = word 
+if current_word == word: 
+    print('%s\t%s' % (current_word, current_count))
